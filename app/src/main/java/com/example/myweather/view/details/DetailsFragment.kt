d1a0765b17b30d39b9caa24887c006421f68a006
AleksandrRogov25 <104849405+AleksandrRogov25@users.noreply.github.com>
2023-03-05 22:22:22 +0300

    Weather lesson5 Сделал загрузку с Яндекс сервера, добавил ключ в файл

--- app/src/main/java/com/example/myweather/view/details/DetailsFragment.kt
+++ app/src/main/java/com/example/myweather/view/details/DetailsFragment.kt
@@ -1,18 +1,33 @@
 package com.example.myweather.view.details
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.myweather.R
 import com.example.myweather.databinding.FragmentDetailsBinding
 import com.example.myweather.model.Weather
+import com.example.myweather.model.WeatherDTO
 
 @Suppress("DEPRECATION")
 class DetailsFragment : Fragment() {
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var weather: Weather
+    private val onLoadListener: WeatherLoader.WeatherLoaderListener =
+        object : WeatherLoader.WeatherLoaderListener {
+            override fun onLoaded(weatherDTO: WeatherDTO) {
+                displayWeather(weatherDTO)
+            }
+
+            override fun onFailed(throwable: Throwable) {
+
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,23 +36,14 @@ class DetailsFragment : Fragment() {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         return binding.getRoot()
     }
-
+@RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        arguments?.getParcelable<Weather>(BUNDLE_EXTRA)?.let { weather ->
-            weather.city.also {
-                with(binding) {
-                    cityName.text = it.city
-                    cityCoordinates.text = String.format(
-                        getString(R.string.city_coordinates),
-                        it.lat.toString(),
-                        it.lon.toString()
-                    )
-                    temperatureValue.text = weather.temperature.toString()
-                    feelsLikeValue.text = weather.feelsLike.toString()
-                }
-            }
-        }
+        weather = arguments?.getParcelable<Weather>(BUNDLE_EXTRA) ?: Weather()
+        binding.mainView.visibility = View.GONE
+        binding.loadingLayout.visibility = View.VISIBLE
+        val loader = WeatherLoader(onLoadListener, weather.city.lat, weather.city.lon)
+        loader.loadWeather()
     }
 
     override fun onDestroyView() {
@@ -51,6 +57,18 @@ class DetailsFragment : Fragment() {
             val fragment = DetailsFragment()
             fragment.arguments = bundle
             return fragment
+        }
+    }
+    private fun displayWeather(weatherDTO: WeatherDTO) {
+        with(binding) {
+            mainView.visibility = View.VISIBLE
+            loadingLayout.visibility = View.GONE
+            val city = weather.city
+            cityName.text = city.city
+            cityCoordinates.text = "${city.lat} ${city.lon}"
+            weatherCondition.text = weatherDTO.fact?.condition
+            temperatureValue.text = weatherDTO.fact?.temp.toString()
+            feelsLikeValue.text = weatherDTO.fact?.feels_like.toString()
         }
     }
 }
