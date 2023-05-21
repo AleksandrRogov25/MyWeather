d886921bec48270ad1333b46bd86a36df1c514ba
AleksandrRogov25 <104849405+AleksandrRogov25@users.noreply.github.com>
2023-05-21 22:35:14 +0300

    Weather lesson9 работа с ContentProvider, запрос на получение контактов

--- app/src/main/java/com/example/myweather/view/MainActivity.kt
+++ app/src/main/java/com/example/myweather/view/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.myweather.MyApp
 import com.example.myweather.R
 import com.example.myweather.databinding.ActivityMainBinding
+import com.example.myweather.lesson9.WorkWithContentProviderFragment
 import com.example.myweather.view.history.HistoryFragment
 import com.example.myweather.view.main.MainFragment
 
@@ -38,6 +39,12 @@ class MainActivity : AppCompatActivity() {
             R.id.menu_history -> {
                 supportFragmentManager.beginTransaction()
                     .add(R.id.container, HistoryFragment.newInstance())
+                    .addToBackStack("")
+                    .commit()
+            }
+            R.id.menu_content_provider -> {
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.container, WorkWithContentProviderFragment.newInstance())
                     .addToBackStack("")
                     .commit()
             }
