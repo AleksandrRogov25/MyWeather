56e5e5849bbad907aa04d8935127720018809036
AleksandrRogov25 <104849405+AleksandrRogov25@users.noreply.github.com>
2023-06-11 23:34:47 +0300

    Weather lesson10 получил разрешение FINE_LOCATION, получил текущую локацию, по ее координатам получил адрес и вывел для него погоду

--- app/src/main/java/com/example/myweather/view/main/MainFragment.kt
+++ app/src/main/java/com/example/myweather/view/main/MainFragment.kt
@@ -1,23 +1,36 @@
 package com.example.myweather.view.main
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.myweather.R
 import com.example.myweather.databinding.FragmentMainBinding
+import com.example.myweather.model.City
 import com.example.myweather.model.Weather
 import com.example.myweather.view.details.DetailsFragment
 import com.example.myweather.viewmodel.AppState
 import com.example.myweather.viewmodel.MainViewModel
+import java.util.TreeMap
 
 private const val IS_WORLD_KEY = "LIST_OF_TOWNS_KEY"
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), MainFragmentAdapter.OnItemViewClickListener {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
@@ -26,7 +39,8 @@ class MainFragment : Fragment() {
         ViewModelProvider(this).get(MainViewModel::class.java)
     }
 
-    private val adapter = MainFragmentAdapter(object : MainFragmentAdapter.OnItemViewClickListener {
+    private val adapter = MainFragmentAdapter(this)
+
         override fun onItemClick(weather: Weather) {
             activity?.supportFragmentManager?.apply {
                 beginTransaction()
@@ -37,7 +51,7 @@ class MainFragment : Fragment() {
                     .commitAllowingStateLoss()
             }
         }
-    })
+
 
     private var isDataSetRus: Boolean = true
 
@@ -64,6 +78,7 @@ class MainFragment : Fragment() {
         with(viewModel) {
             getData().observe(viewLifecycleOwner, observer)
             getWeatherRussia()
+            setupFabLocation()
         }
         showListOfTowns()
     }
@@ -112,6 +127,12 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    private fun setupFabLocation() {
+        binding.mainFragmentFABLocation.setOnClickListener() {
+            checkPermission()
+        }
+    }
+
     private fun changeWeatherDataSet() {
         if (isDataSetRus) {
             viewModel.getWeatherWorld()
@@ -122,5 +143,144 @@ class MainFragment : Fragment() {
         }
         isDataSetRus = !isDataSetRus
         saveListOfTowns()
+    }
+
+    fun checkPermission() {
+
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) ==
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            getLocation()
+        } else if (shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)) {
+            explan()
+        } else {
+            mRequestPermission()
+        }
+    }
+
+
+    fun getAddressByLocation(location: Location) {
+        val geocoder = Geocoder(requireContext())
+        val timeStump = System.currentTimeMillis()
+        Thread {
+            val addressText =
+                geocoder.getFromLocation(location.latitude, location.longitude, 1000000)!![0].getAddressLine(0)
+           requireActivity().runOnUiThread() {
+               showAddressDialog(addressText, location)
+           }
+        }.start()
+            Log.d("@@@", "прошло ${System.currentTimeMillis() - timeStump}")
+    }
+
+    private val locationListenerTime = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Log.d("@@@", location.toString())
+            getAddressByLocation(location)
+
+
+        }
+
+        override fun onProviderDisabled(provider: String) {
+            super.onProviderDisabled(provider)
+        }
+
+        override fun onProviderEnabled(provider: String) {
+            super.onProviderEnabled(provider)
+        }
+    }
+
+    private val locationListenerDistance = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Log.d("@@@", location.toString())
+            getAddressByLocation(location)
+        }
+
+        override fun onProviderDisabled(provider: String) {
+            super.onProviderDisabled(provider)
+        }
+
+        override fun onProviderEnabled(provider: String) {
+            super.onProviderEnabled(provider)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        context?.let {
+            val locationManager = it.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                val providerGPS = locationManager.getProvider(LocationManager.GPS_PROVIDER)
+              /*  providerGPS?.let {
+                    locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        10000L,
+                        0f,
+                        locationListenerTime
+                    )
+                }*/
+                providerGPS?.let {
+                    locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        0L,
+                        100f,
+                        locationListenerDistance
+                    )
+                }
+            }
+        }
+    }
+
+    fun explan() {
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.dialog_rationale_title))
+            .setMessage(getString(R.string.dialog_rationale_meaasge))
+            .setPositiveButton(getString(R.string.dialog_rationale_give_access)) { _, _ ->
+                mRequestPermission()
+            }
+            .setNegativeButton(getString(R.string.dialog_rationale_decline)) { dialog, _ -> dialog.dismiss() }
+            .create()
+            .show()
+    }
+
+    val REQUEST_CODE = 998
+    private fun mRequestPermission() {
+        requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE) {
+            for (i in permissions.indices) {
+                if (permissions[i] == Manifest.permission.ACCESS_FINE_LOCATION && grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation()
+                } else {
+                    explan()
+                }
+            }
+
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        }
+    }
+    private fun showAddressDialog(address: String, location: Location) {
+        activity?.let {
+            AlertDialog.Builder(it)
+                .setTitle(getString(R.string.dialog_address_title))
+                .setMessage(address)
+                .setPositiveButton(getString(R.string.dialog_address_get_weather)){_, _ ->
+                   onItemClick(Weather(City(address,location.latitude,location.longitude)))
+                }
+                .setNegativeButton(getString(R.string.dialog_button_close)) {dialog, _ -> dialog.dismiss()}
+                .create()
+                .show()
+
+        }
     }
 }
